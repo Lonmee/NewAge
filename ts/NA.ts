fd6f4ecb71f36e11df7d4b5144461277e508828f
Lonmee <Lonmee@126.com>
2018-03-20 17:09:52 +0800

    开启webgl

--- ts/NA.ts
+++ ts/NA.ts
@@ -5,8 +5,8 @@
 import {Conf} from "./mgr/data/Conf";
 import {Interactiver} from "./mgr/Interactiver";
 import {BD} from "./mgr/data/BD";
-import {ReduxTest} from "./mgr/test/ReduxTest";
 import {BindTest} from "./mgr/test/BindTest";
+import WebGL = laya.webgl.WebGL;
 
 export class NA {
     constructor() {
@@ -17,7 +17,7 @@ export class NA {
             window['na'] = this;
         }
 
-        Laya.init(Conf.stageConf.width, Conf.stageConf.height/*, WebGL*/);
+        Laya.init(Conf.stageConf.width, Conf.stageConf.height, WebGL);
         Laya.stage.scaleMode = Conf.stageConf.scaleMode;
         Laya.stage.bgColor = Conf.stageConf.bgColor;
 
